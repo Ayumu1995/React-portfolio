880ced4b0d40004c976110e7c44bc4335ddc0715
Ayumu1995 <297979@student.tamwood.com>
2025-05-08 04:16:54 -0700

    Change order in mobile view

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -45,7 +45,7 @@ const Hero = () => {
 
    return (
       <div className="pb-4 lg:mb-36">
-         <div className="flex flex-wrap lg:flex-row-reverse">
+         <div className="flex flex-wrap flex-col-reverse lg:flex-row-reverse">
             <div className="w-full lg:w-1/2">
                <div className="flex justify-center lg:p-8">
                   <motion.img
